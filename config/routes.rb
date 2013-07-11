98ffcd3d90812f8cad1278c4ab52f873ab57e759
djerek <derekgritz@yahoo.com>
2013-07-11 16:58:16 -0400

    routes and must authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Project2::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,6 +8,13 @@ Project2::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root :to => "users#index"
+
+  as :user do
+    get "login" => "devise/sessions#new", :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    match "logout" => "devise/sessions#destroy", :as => :destroy_user_session,
+      :via => Devise.mappings[:user].sign_out_via
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
