771f748edc1e00715a7f1d40bf974a345a210228
djerek <derekgritz@yahoo.com>
2013-07-11 16:37:56 -0400

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Project2::Application.routes.draw do
+  devise_for :users
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root :to => "users#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
